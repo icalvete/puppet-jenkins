f923f99c95b5cc7c917ca41c34b4bbf960afe74a
Israel Calvete <icalvete@gmail.com>
2017-03-12 17:22:01 +0100

    Fix travis error.

--- manifests/plugins.pp
+++ manifests/plugins.pp
@@ -3,7 +3,7 @@ class jenkins::plugins {
   if is_array($jenkins::plugins) {
     $plugins = split(inline_template("<%= (scope.lookupvar('jenkins::plugins')+scope.lookupvar('jenkins::params::plugins')).join(',') %>"),',')
   } else {
-    $plugins = $jenkins::params::plugins 
+    $plugins = $jenkins::params::plugins
   }
 
   jenkins::plugin{$plugins:}
